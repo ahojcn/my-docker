03cd57628f17e73bd60eddaa69cfd41c5bd2f7b8
ahojcn <ahojcn@gmail.com>
2020-02-29 19:43:33 +0800

    5-3 实现用户进程为1号进程

--- README.md
+++ README.md
@@ -11,6 +11,13 @@ $ ./mydocker run -it /bin/sh
 
 tags:
 
-5-1:实现 run 方法
+5-1:实现 run 方法。
 
-5-2:实现 init 方法（在容器起来前先执行 mount -t proc proc /proc）
+5-2:实现 init 方法（在容器起来前先执行 mount -t proc proc /proc）。
+
+5-3:实现用户进程为1号进程。
+因为5-2的程序运行后，1号进程并不是我们要的 sh，而是 mount 操作使用的一个进程。
+
+---
+
+EOF
--- command/init.go
+++ command/init.go
@@ -1,9 +1,8 @@
 package command
 
 import (
-	"log"
+	log "github.com/Sirupsen/logrus"
 	"os"
-	"os/exec"
 	"syscall"
 )
 
@@ -11,6 +10,8 @@ func Init(command string) {
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
 
+	/**
+	// 5-2 直接 cmd.Run() 会导致 1 号进程不是用户进程
 	cmd := exec.Command(command)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -18,5 +19,15 @@ func Init(command string) {
 
 	if err := cmd.Run(); err != nil {
 		log.Fatalln("Init Run() error", err)
+	}
+	*/
+
+	/**
+	exec 函数族
+	syscall.Exec 会先执行参数指定的命令，但是并不创建新的进程，只在当前进程空间内执行
+		即替换当前进程的执行内容，会重用同一个进程PID
+	*/
+	if err := syscall.Exec(command, []string{command}, os.Environ()); err != nil {
+		log.Fatalln("Init syscall.Exec() error", err)
 	}
 }
