2487e3263655f9d2b6b5c84acf6e1e981c9aa72b
ahojcn <ahojcn@gmail.com>
2020-03-01 21:43:21 +0800

    11-1 使用 busybox 作为容器跟目录

--- command/init.go
+++ command/init.go
@@ -1,9 +1,11 @@
 package command
 
 import (
+	"fmt"
 	log "github.com/Sirupsen/logrus"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"syscall"
 )
 
@@ -11,6 +13,14 @@ func Init(command string) {
 	log.Infoln("read from commandline:", command)
 	command = readFromPipe()
 	log.Infoln("read from pipe:", command)
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Errorln("get pwd error:", err)
+		return
+	}
+	log.Infoln("current path:", pwd)
+	pivotRoot(pwd)
 
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
@@ -26,13 +36,6 @@ func Init(command string) {
 		log.Fatalln("Init Run() error", err)
 	}
 	*/
-
-	pwd, err := os.Getwd()
-	if err != nil {
-		log.Errorln("get pwd error:", err)
-		return
-	}
-	log.Infoln("current path:", pwd)
 
 	/**
 	exec 函数族
@@ -52,4 +55,39 @@ func readFromPipe() string {
 		return ""
 	}
 	return string(command)
+}
+
+/**
+把路径 root 变为根节点
+*/
+func pivotRoot(root string) error {
+	/**
+	  为了使当前root的老 root 和新 root 不在同一个文件系统下，我们把root重新mount了一次
+	  bind mount是把相同的内容换了一个挂载点的挂载方法
+	*/
+	if err := syscall.Mount(root, root, "bind", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+		return fmt.Errorf("Mount root fs to itself error: %v", err)
+	}
+	// 创建 rootfs/.pivot_root 存储 old_root
+	pivotDir := filepath.Join(root, ".pivot_root")
+	if err := os.Mkdir(pivotDir, 0777); err != nil {
+		return err
+	}
+	// pivot_root 到新的 rootfs, 现在老的 old_root 是挂载在 rootfs/.pivot_root
+	// 挂载点现在依然可以在 mount 命令中看到
+	if err := syscall.PivotRoot(root, pivotDir); err != nil {
+		return fmt.Errorf("pivot_root: %v", err)
+	}
+	// 修改当前的工作目录到根目录
+	if err := syscall.Chdir("/"); err != nil {
+		return fmt.Errorf("chdir / %v", err)
+	}
+
+	pivotDir = filepath.Join("/", ".pivot_root")
+	// umount rootfs/.pivot_root
+	if err := syscall.Unmount(pivotDir, syscall.MNT_DETACH); err != nil {
+		return fmt.Errorf("unmount pivot_root dir error: %v", err)
+	}
+	// 删除临时文件夹
+	return os.Remove(pivotDir)
 }
